4ba4b52ad1094abfc0f601e5675a276667aa990d
Deivy <deivy.munoz-acosta@mohawkcollege.ca>
2023-07-10 15:00:03 -0400

    Manually square selection

--- video_cropper.py
+++ video_cropper.py
@@ -1,18 +1,25 @@
 import cv2 as cv
 
-def square(event, x, y, flags, params):
-    # global frame
-    if event == cv.EVENT_LBUTTONDOWN:
-        cv.circle(frame,(x,y),2,(0,0,255),-1)
-        cv.imshow('Frame', frame)
-        print(x,y)
+squares = []
+square = []
+
+def adding_points(event, x, y, flags, params):
+  global squares, square
+  if event == cv.EVENT_LBUTTONDOWN:
+    square.append([x,y])
+    if len(square)%2 == 0:
+      squares.append([square[-1], square[-2]])
+      print(squares)
+
+    
 
 cap = cv.VideoCapture('Media1.mp4')
 
 paused = False
+refresh = True
 
 cv.namedWindow('Frame')
-cv.setMouseCallback('Frame', square)
+cv.setMouseCallback('Frame', adding_points)
  
 # Check if camera opened successfully
 if cap.isOpened()== False: 
@@ -21,21 +28,24 @@ if cap.isOpened()== False:
 # Read until video is completed
 while cap.isOpened():
   # Capture frame-by-frame
-    if not paused:
-        ret, frame = cap.read()
-        if not ret:
-            break
+  if not paused:
+    refresh = True
+    ret, frame = cap.read()
+    if not ret:
+      break
 
     # Display the resulting frame
-    cv.imshow('Frame', frame)
+  for square_to_draw in squares:
+    cv.rectangle(frame, square_to_draw[0], square_to_draw[1], (0,0,255))
+  cv.imshow('Frame', frame)
     
     
     # Press Q on keyboard to  exit
-    key = cv.waitKey(25)
-    if key == ord('q'):
-        exit(0)
-    if key == ord('p'):
-        paused = not paused
+  key = cv.waitKey(1)
+  if key == ord('q'):
+    exit(0)
+  if key == ord('p'):
+    paused = not paused
  
 # When everything done, release the video capture object
 cap.release()
