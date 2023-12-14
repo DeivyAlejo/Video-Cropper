8d3406e49fc919c4acd9fcd7b4810ad99480282e
Deivy <deivy.munoz-acosta@mohawkcollege.ca>
2023-12-13 23:18:48 -0500

    showing crops added

--- video_cropper.py
+++ video_cropper.py
@@ -8,9 +8,13 @@ def adding_points(event, x, y, flags, params):
   if event == cv.EVENT_LBUTTONDOWN:
     square.append([x,y])
     if len(square)%2 == 0:
-      squares.append([square[-1], square[-2]])
-      print(squares)
+      squares.append([square[-2], square[-1]])
+      # print(squares)
 
+def imgcrop(frame, rectangle_points):
+  point1 = rectangle_points[0]
+  point2 = rectangle_points[1]
+  return frame[point1[1]:point2[1], point1[0]:point2[0]]
     
 
 cap = cv.VideoCapture('Media1.mp4')
@@ -31,12 +35,18 @@ while cap.isOpened():
   if not paused:
     refresh = True
     ret, frame = cap.read()
+    copy_frame = frame.copy()
     if not ret:
       break
 
     # Display the resulting frame
-  for square_to_draw in squares:
-    cv.rectangle(frame, square_to_draw[0], square_to_draw[1], (0,0,255))
+  for index, square_to_draw in enumerate(squares):
+    # cv.rectangle(frame, square_to_draw[0], square_to_draw[1], (0,0,255))
+    # image_cropped = frame[197:829, 7:777]
+    image_cropped = imgcrop(frame, square_to_draw)
+    image_name = f"Image Cropped {index}"
+    cv.imshow(image_name, image_cropped)
+    # print(square_to_draw)
   cv.imshow('Frame', frame)
     
     
@@ -46,6 +56,7 @@ while cap.isOpened():
     exit(0)
   if key == ord('p'):
     paused = not paused
+  
  
 # When everything done, release the video capture object
 cap.release()
